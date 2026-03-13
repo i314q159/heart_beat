1b0b562cb7bea29487b25e7aea845b0162c4b574
i314q159 <i314q159@outlook.com>
2026-03-13 22:45:37 +0800

    Output builds to heartbeat dir and zip artifacts

--- build.ps1
+++ build.ps1
@@ -1,2 +1,4 @@
-$env:GOOS="linux"; $env:GOARCH="amd64"; go build -o heart_beat_amd64 main.go
-$env:GOOS="linux"; $env:GOARCH="arm64"; go build -o heart_beat_arm64 main.go
+$env:GOOS="linux"; $env:GOARCH="amd64"; go build -o heartbeat/heart_beat_amd64 main.go
+$env:GOOS="linux"; $env:GOARCH="arm64"; go build -o heartbeat/heart_beat_arm64 main.go
+
+Compress-Archive -Path ".\heartbeat" -DestinationPath ".\heartbeat.zip" -Update
