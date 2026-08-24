a24cfbfd6e96b0dfdb40ef7e95a3d7c7769fb176
i314q159 <i314q159@outlook.com>
2026-08-24 08:52:30 +0800

    update

--- build.ps1
+++ build.ps1
@@ -1,4 +1,2 @@
 $env:GOOS="linux"; $env:GOARCH="amd64"; go build -o heartbeat/heart_beat_amd64 main.go
 $env:GOOS="linux"; $env:GOARCH="arm64"; go build -o heartbeat/heart_beat_arm64 main.go
-
-Compress-Archive -Path ".\heartbeat" -DestinationPath ".\heartbeat.zip" -Update
