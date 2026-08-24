39f7248f893bcd30d85ac1c16d75265cb517b098
i314q159 <i314q159@outlook.com>
2026-08-24 09:13:04 +0800

    update build

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ mkdir -pv ./heartbeat
 
 GOOS=linux GOARCH=amd64 go build -o ./heartbeat/heart_beat_amd64 main.go
 GOOS=linux GOARCH=arm64 go build -o ./heartbeat/heart_beat_arm64 main.go
+
+zip -r heartbeat.zip heartbeat/
